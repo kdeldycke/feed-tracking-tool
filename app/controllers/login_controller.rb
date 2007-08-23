7bef5d2cdd32f7ec533f8eeba55f49501e85ce14
Kevin Deldycke <kevin@deldycke.com>
2007-08-23 16:38:16 +0000

    LDAP is now the default authentication method

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,52 +1,34 @@
-#require 'ldap_connect'
+require 'ldap_connect'
 class LoginController < ApplicationController
-  skip_before_filter :authenticate  # Permet l'accès à cette page sans être authentifié
-  
+  skip_before_filter :authenticate  # Permet l'accï¿½s ï¿½ cette page sans ï¿½tre authentifiï¿½
+
   def index
-    
+
   end
-  
-  # Authentification
-#  def authenticate
-#    if session[:person] = LDAP.authenticate(params[:login][:name], params[:login][:password])
-#      session[:username] = params[:login][:name].capitalize_each
-#      if session[:return_to]
-#        redirect_to_path(session[:return_to])
-#        session[:return_to] = nil
-#      else
-#        redirect_to :controller => "dashboard", :action => "dashboard"
-#      end
-#    else
-#      flash[:notice] = "Echec de l'authentification!"
-#      redirect_to :action => "index"
-#    end
-#  end
-  
+
   def authenticate
-    if request.post?
+    # Inspired by http://www.rich-waters.com/blog/2007/02/using-domino-logins-ldap-for-a-ruby-on-rails-app.html
+    auth_result = LDAP.authenticate(params[:login][:name], params[:login][:password])
+    if auth_result[:authenticated]
       session[:username] = params[:login][:name]
-      #Notifier.deliver_test_email(Profile.find_by_user_id(session[:username]).email)
-      redirect_to :controller => "dashboard", :action => "display"
+      if session[:return_to]
+        redirect_to_path(session[:return_to])
+        session[:return_to] = nil
+      else
+        redirect_to :controller => "dashboard", :action => "display"
+      end
+    else
+      flash[:warning] = auth_result[:message]
+      redirect_to :action => "index"
     end
   end
-  
+
   # Logout
-  # réinitialise la session
-#  def logout
-#    session[:person] = nil
-#    session[:username] = nil
-#    flash[:notice] = "Deconnecte"
-#    redirect_to :action => "index"
-#  end
-  
+  # rï¿½initialise la session
   def logout
     session[:username] = nil
-    flash[:notice] = "D&eacute;connect&eacute;"
+    flash[:notice] = "Deconnecte"
     redirect_to :action => "index"
   end
-  
-  def profile
-    
-  end
-  
+
 end
--- lib/ldap_connect.rb
+++ lib/ldap_connect.rb
@@ -4,23 +4,46 @@ require 'net/ldap'
 
 module LDAP
 
-  def self.authenticate(user, password)
-    if user == "" || password == ""
-      return false
+  def self.authenticate(login, password)
+    # Init default returned hash
+    result = {:authenticated => false, :message => nil}
+
+    # Check login and password
+    if login.length == 0
+      result[:message] = "No login"
+      return result
+    elsif password.length == 0
+      result[:message] = "No password"
+      return result
     end
-    ldap_con = Net::LDAP.new({:host => '12.34.56.78',
-                              :port => 389,
-                              :base => "dc=uperto",
-                              :auth => {:method => :simple,
-                                        :username => "uid=#{user},ou=users,dc=uperto",
-                                        :password => password
-                            }})
 
-#     logger.info "Result: #{truc.get_operation_result.code}"
-#     logger.info "Message: #{truc.get_operation_result.message}"
+    # Setup the LDAP connection
+      ldap_con = Net::LDAP.new({:host => '12.34.56.78',
+                                :port => 389,
+                                :base => "dc=uperto",
+                                :auth => {:method   => :simple,
+                                          :username => "uid=#{login},ou=users,dc=uperto",
+                                          :password => password
+                              }})
 
-    return true if ldap_con.bind
-    return false
+    # Connect to server
+    begin
+      ldap_con.bind
+    rescue => e
+      result[:msg] = e.message
+      return result
+    end
+
+    con_result = ldap_con.get_operation_result
+    if con_result.code != 0
+      result[:message] = "LDAP error ##{con_result.code}: #{con_result.message}"
+      return result
+    end
+
+    # User authenticated !
+    result[:authenticated] = true
+    result[:message] = con_result.message
+    return result
   end
 
 end
