cd40eb6e1ec63d5e72e30c3b7ea1accdd08b128a
Kevin Deldycke <kevin@deldycke.com>
2007-08-24 14:43:14 +0000

    Constraint on user_id is too restrictive actually to let us store LDAP full DN

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,9 +2,9 @@ class Profile < ActiveRecord::Base
   validates_presence_of :user_id
   validates_uniqueness_of :user_id
   # TODO: check against LDAP capabilities and character tolerance
-  validates_format_of :user_id,
-                      :with    => %r{^[a-zA-Z0-9]*}i,
-                      :message => "must be a-Z and/or 0-9, no spaces or any other characters"
+  # validates_format_of :user_id,
+  #                     :with    => %r{^[a-zA-Z0-9]*}i,
+  #                     :message => "must be a-Z and/or 0-9, no spaces or any other characters"
   # TODO: make the validation regexp match the RFC 822: http://www.freesoft.org/CIE/RFC/822/index.htm
   #     alt: http://tfletcher.com/lib/rfc822.rb
   # Suggested by http://nshb.net/rails-validates_format_of-email-email_address-regular_expression-php_option_too
