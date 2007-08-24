01209912fe858007eebe7ff3fad5551f63853c9a
Kevin Deldycke <kevin@deldycke.com>
2007-08-24 15:21:22 +0000

    Validate feed url before adding it to database.

--- app/models/rssfeed.rb
+++ app/models/rssfeed.rb
@@ -1,4 +1,8 @@
 class Rssfeed < ActiveRecord::Base
   has_many :trackers
-  # TODO: validate URL using code from http://www.nshb.net/node/252 article
+  # Inspired by http://www.nshb.net/node/252
+  # We should use a combination of FeedTools nomalize method (which take care of URL of the rss:// and feed:// form) and http://www.igvita.com/blog/2006/09/07/validating-url-in-ruby-on-rails/
+  validates_format_of :url,
+                      :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix, # Only accept http(s) URLs
+                      :message => "invalid."
 end
