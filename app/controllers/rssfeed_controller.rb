0f0e513e56a34b0099d26f2c432dc7dfccc13d21
Kevin Deldycke <kevin@deldycke.com>
2007-08-29 09:43:14 +0000

    Use FeedTools for feed parsing. Delete other feed parsing method. Convert charset to UTF-8.

--- app/controllers/rssfeed_controller.rb
+++ app/controllers/rssfeed_controller.rb
@@ -1,70 +1,38 @@
 class RssfeedController < ApplicationController
 
   def manage
-    @rssfeed = Rssfeed.new(params[:rssfeed])  # Création d'une entrée dans la table rssfeed
-    if request.post? and @rssfeed.save        # si le formulaire a été rempli et validé
-      rss(@rssfeed.url)                   # Appel de la méthode de parsing de flux RSS avec l'url saisie
+    @rssfeed = Rssfeed.new(params[:rssfeed])  # Crï¿½ation d'une entrï¿½e dans la table rssfeed
+    if request.post? and @rssfeed.save        # si le formulaire a ï¿½tï¿½ rempli et validï¿½
+      rss(@rssfeed.url)                   # Appel de la mï¿½thode de parsing de flux RSS avec l'url saisie
       @rssfeed.update_attribute :title , @title               # Ajout dans la table rssfeed du champ titre
       @rssfeed.update_attribute :description, @description    # Ajout dans la table rssfeed du champ description
       @rssfeed.update_attribute :link, @link                  # Ajout dans la table rssfeed du champ link
-      flash[:notice] = "Flux RSS ajout&eacute; avec succ&egrave;s. Ce flux sera traité dans l'heure"
+      flash[:notice] = "Flux RSS ajout&eacute; avec succ&egrave;s. Ce flux sera traitï¿½ dans l'heure"
       redirect_to :controller => 'rssfeed', :action => 'manage' # actualisation de la page
     end
   end
-  
-  # Methode pour lire des flux RSS
-  # Ne lit que les Flux de type XML
+
+  # Methode alternative utilisant le parser FeedTools (non utilisï¿½e car problï¿½me de rï¿½glage du proxy)
+  # FeedTools est censï¿½ lire des flux RSS et ATOM
   def rss(url)
-    open(url, :proxy => "http://12.34.56.78:8080") do |http|  # Ouverture de l'url en passant par le proxy
-      response = http.read                                    # Lecture de l'url
-      result = RSS::Parser.parse(response, false)             # Parsing du flux RSS
-      @title = "#{result.channel.title}"                      # Récupération du champ title
-      @description = "#{result.channel.description}"          # Récupération du champ description
-      @link = "#{result.channel.link}"                        # Récupération du champ link
-    end  
-  end
-  
-  
-  # Methode alternative utilisant le parser FeedTools (non utilisée car problème de réglage du proxy)
-  # FeedTools est censé lire des flux RSS et ATOM
-  def rss2(url)
-    #FeedTools.load_configurations
-    #@configurations = { :proxy_address => 'http://12.34.56.78',
-    #                    :proxy_port => 8080 }
-    
-    #FeedTools.configurations[:proxy_address] = 'http://12.34.56.78'
-    #FeedTools.configurations[:proxy_port] = 8080
-    
-    #feed = FeedTools::Feed.open(url, { :proxy_address => "http://12.34.56.78", :proxy_port => 8080 })
-    
     feed = FeedTools::Feed.open(url)
-
-    @title = "#{feed.title}"                      # Récupération du champ title
-    @description = "#{feed.description}"          # Récupération du champ description
-    @link = "#{feed.link}"                        # Récupération du champ link  
+    @title = "#{feed.title}"                      # Rï¿½cupï¿½ration du champ title
+    @description = "#{feed.description}"          # Rï¿½cupï¿½ration du champ description
+    @link = "#{feed.link}"                        # Rï¿½cupï¿½ration du champ link
   end
-  
-  #Méthode utilisant Simple-rss
-  def rss3(url)
-    feed = SimpleRSS.parse open(url, :proxy => "http://12.34.56.78:8080")
 
-    @title = "#{feed.channel.title}"                      # Récupération du champ title
-    @description = "#{feed.channel.description}"          # Récupération du champ description
-    @link = "#{feed.channel.link}"                        # Récupération du champ link  
-  end
-  
   #Suppression d'un flux de la base
   def destroy
-    r=Rssfeed.find(params[:id])   # recherche dans la base de données rssfeed du flux à supprimer
-    
-    d = r.trackers_count        # on compte le nombre de suivis auxquels appartient le flux à supprimer
+    r=Rssfeed.find(params[:id])   # recherche dans la base de donnï¿½es rssfeed du flux ï¿½ supprimer
+
+    d = r.trackers_count        # on compte le nombre de suivis auxquels appartient le flux ï¿½ supprimer
     if d>0                      # si ce nombre est positif, on ne peut pas supprimer le flux
       flash[:warning] = 'Flux RSS utilis&eacute dans un ou plusieurs suivis : Suppression interdite tant que le ou les suivis existent'
     else
       r.destroy                     # suppression
       r.save                        # sauvegarde
       flash[:notice] = 'Flux RSS supprim&eacute; avec succ&egrave;s'
-    end 
+    end
     redirect_to :controller => 'rssfeed', :action => 'manage' # actualisation de la page
   end
 
