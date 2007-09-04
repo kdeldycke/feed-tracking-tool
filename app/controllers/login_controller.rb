447505cc24fe7e7b60e7ce6bee1e49a321909eb1
Kevin Deldycke <kevin@deldycke.com>
2007-09-04 14:15:37 +0000

    Don't send a LDAP request to get more details if not needed. Clean-up code layout and comments.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,7 @@ require 'ldap_connect'
 
 
 class LoginController < ApplicationController
-  skip_before_filter :authenticate  # This page is public
+  skip_before_filter :authenticate   # This page is public
 
   def index
     # Redirect to dashboard if user logged in
@@ -11,36 +11,47 @@ class LoginController < ApplicationController
     end
   end
 
+
   def authenticate
+
     # Inspired by http://www.rich-waters.com/blog/2007/02/using-domino-logins-ldap-for-a-ruby-on-rails-app.html
     login = params[:login][:name]
     pwd   = params[:login][:password]
     dn    = LDAP.generateDNFromLogin(login)
     auth_result = LDAP.authenticateByDN(dn, pwd)
+
     if auth_result[:authenticated]
-      user_details = LDAP.getAttributeList(login, pwd)
-      # Create new profile and update default mail if necessary
+
+      # Create new profile if first login (= no user found in database), else load user profile
       profile = Profile.find_by_user_id(dn)
       if not profile:
         profile = Profile.new
         profile.update_attribute :user_id, dn
       end
-      if profile.email.blank?
-        profile.update_attribute :email, user_details[:mail]
-      end
+
+      # Initialize each user preference if first login
       if profile.suspend_email.blank?
         # Activate email sending by default. If the user receive mails he don't want, a message in each email footer remind him how to disable automatic mail sending.
         profile.update_attribute :suspend_email, false
       end
-      if profile.display_name.blank?
-        # TODO: do not get display_name or email adress from the LDAP if not empty
-        profile.update_attribute :display_name, user_details[:displayname]
+
+      # Do we need a LDAP request to get more details ?
+      if profile.email.blank? or profile.display_name.blank?
+        user_details = LDAP.getAttributeList(login, pwd)   # get all user LDAP details
+        if profile.email.blank?
+          profile.update_attribute :email, user_details[:mail]
+        end
+        if profile.display_name.blank?
+          profile.update_attribute :display_name, user_details[:displayname]
+        end
       end
+
       # Register user in the session
       session[:user] = {:login        => login,
                         :id           => dn,
                         :display_name => Profile.find_by_user_id(dn).display_name
                        }
+
       # Redirect to the right place
       if session[:return_to]
         redirect_to_path(session[:return_to])
