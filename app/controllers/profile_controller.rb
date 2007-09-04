6e1f8a164e7289ef02d099679cbdaafa1f6d4a75
Kevin Deldycke <kevin@deldycke.com>
2007-09-04 16:04:04 +0000

    Update session display name only if the profile was saved sucessfully. Update comment. Add TODO statements.

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,6 +2,7 @@ class ProfileController < ApplicationController
 
   def edit
     # Get the currrent user profile or create a new one if doesn't exist
+    # XXX Duplicate code with the one which exist in the login_controller !
     user_id = session[:user][:id]
     profile = Profile.find_by_user_id(user_id)
     if not profile:
@@ -15,12 +16,13 @@ class ProfileController < ApplicationController
       profile.email = params[:profile][:email]
       profile.suspend_email = params[:profile][:suspend_email]
       profile.display_name = params[:profile][:display_name]
-      # Update session with new user's dispay_name
-      session[:user][:display_name] = profile.display_name
+      # TODO: if one of the variable is empty, get LDAP default value like login_controller do. If so, warn the user with this following message: flash[:notice] = 'Preferences updated. Display name resetted to LDAP default.' I think all this stuff must be generic enough to not repeat ourselves and to be put in profile_helper...
       # Try to save the new profile in the database
       if profile.save
         flash[:notice] = 'Preferences updated'
-      # Database save failed, new profile data don't fit in the model.
+        # Update session with new user's dispay_name
+        session[:user][:display_name] = profile.display_name
+      # Database save failed, new profile data do not respect model data constraint.
       else
         # TODO: get error message from the profile model constraint definition
         flash[:warning] = 'Invalid email address !'
@@ -36,7 +38,7 @@ class ProfileController < ApplicationController
 
 
   def users
-    @users = Subscription.find(:all, :select => 'DISTINCT user_id' )        # Gets list of users in subscription table
+    @users = Subscription.find(:all, :select => 'DISTINCT user_id')  # Gets list of users in subscription table
   end
 
 end
