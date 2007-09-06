879388c954407da3545b5b843ccb8aaf1d740043
Quentin Desert <quentin.desert@uperto.com>
2007-09-06 09:44:34 +0000

    Manage Feeds: User can now enter anything when adding a feed, adequat error message will be displayed. Existing feeds are sorted by title.

--- app/controllers/rssfeed_controller.rb
+++ app/controllers/rssfeed_controller.rb
@@ -3,12 +3,16 @@ class RssfeedController < ApplicationController
   def manage
     rssfeed = Rssfeed.new(params[:rssfeed])  # Creation of an entry in rssfeed table
     if request.post?
-      if rss(rssfeed.url) == true # If the feed from the url entered in the form can be parsed
-        if rssfeed.save       # If the form has been validated
+      if rssfeed.save       # If the form has been validated
+        if rss(rssfeed.url) == true # If the feed from the url entered in the form can be parsed
           rssfeed.update_attribute :title , @title               # The title field is updated in the rssfeed table
           rssfeed.update_attribute :description, @description    # The description field is updated in the rssfeed table
           rssfeed.update_attribute :link, @link                  # The link field is updated in the rssfeed table
-          flash[:notice] = "RSS feed added successfully. This feed will be taken into account within the hour."
+          flash[:notice] = "Feed added successfully. This feed will be taken into account within the hour. You can track this feed in 'Trackers'."
+        else
+          flash[:warning] = "Invalid URL!"
+          rssfeed.destroy
+          rssfeed.save
         end
       else
         flash[:warning] = "Invalid URL!"
@@ -22,8 +26,8 @@ class RssfeedController < ApplicationController
   def rss(url)
     ret = false
     feed = FeedTools::Feed.open(url)  # Creation of an instance of FeedTools and opening of the feed
-    # If the parsing return nil fields, the feed is not valid
-    unless feed.title.nil? and feed.description.nil? and feed.link.nil?
+    # If the parsing return nil title field, we consider the feed not valid
+    unless feed.title.nil?
       @title       = feed.title        # We get the title field and convert it to unicode if needed
       @description = feed.description  # We get the description field and convert it to unicode if needed
       @link        = feed.link         # We get the link field
@@ -38,11 +42,11 @@ class RssfeedController < ApplicationController
 
     d = r.trackers_count        # We count the number of trackers associated to this feed
     if d>0                      # If this number is positive, we can't remove the RSS feed
-      flash[:warning] = 'RSS feed used in one ore more trackers : Removing it is forbidden until tracker(s) exists.'
+      flash[:warning] = 'Feed used in one ore more trackers : Removing it is forbidden until tracker(s) exists.'
     else
       r.destroy                     # Destroying the feed
       r.save
-      flash[:notice] = 'RSS feed removed successfully!'
+      flash[:notice] = 'Feed removed successfully!'
     end
     redirect_to :controller => 'rssfeed', :action => 'manage' # Refreshing page
   end
