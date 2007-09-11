18941eda5829b7343c13f0406a8485cbc55794f5
Kevin Deldycke <kevin@deldycke.com>
2007-09-11 11:53:22 +0000

    New error message: cleaner

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -19,7 +19,7 @@ class TrackerController < ApplicationController
       # Link the selected feed with our tracker
       @selected_feed = params[:rssfeed][:id]
       @tracker.update_attribute :rssfeed_id, @selected_feed          # Update of feed_id field in rssfeed table
-      flash[:notice] = "Tracker added successfully. You can subscribe to this tracker through 'My Trackers'."
+      flash[:notice] = "New tracker created. You can subscribe to this tracker in 'My Subscriptions'."
       redirect_to :controller => 'tracker', :action => 'edit'     # Refreshing page
     end
   end
@@ -30,11 +30,11 @@ class TrackerController < ApplicationController
 
     d = t.subscriptions_count   # We count the number of subscriptions to this tracker
     if d>0                      # If this number is positive, we can't remove the tracker
-      flash[:warning] = 'One or more users have subscribed to this tracker : Removing it is forbidden until subscription(s) exists.'
+      flash[:warning] = "Other users are still using this tracker: you can't remove it !"
     else
       t.destroy                     # Destroying the tracker
       t.save
-      flash[:notice] = 'Tracker deleted successfully!'
+      flash[:notice] = 'Tracker deleted.'
     end
     redirect_to :controller => 'tracker', :action => 'edit'     # Refreshing page
   end
