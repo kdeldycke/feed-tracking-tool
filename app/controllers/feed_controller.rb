d29cff338d73bb70b66c0c1783cb2b5f8d7106c7
Kevin Deldycke <kevin@deldycke.com>
2007-09-20 15:26:17 +0000

    Major refactoring of feed model and controller to add support of static pages. This is done through a feedalize service that is callable through the URL.

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,56 +1,94 @@
 class FeedController < ApplicationController
 
+
   # Default method to view a flat list of all feed in the database
   def index
   end
 
+
   # Method to add a new feed in the database
   def add
-    feed = Feed.new(params[:feed])  # Creation of an entry in feed table
     if request.post?
-      # We check if the entered feed already exists in the database
-      ex = false
-      Feed.find(:all).each do |r|
-        if feed.url == r.url
-          ex = true
+      feed = Feed.new(params[:feed])  # Create a temporary local feed
+
+      if feed.valid?  # True if object respect model constraints
+        begin
+          f = FeedTools::Feed.open(feed.url)  # Let FeedTools try to do its best to get the feed
+        rescue FeedTools::FeedAccessError => e
+
+          e.message
+
         end
-      end
-      if ex == false  # If the feed doesn't exist in the database
-        if feed.save       # If the form has been validated
-          if rss(feed.url) == true # If the feed from the url entered in the form can be parsed
-            feed.update_attribute :title , @title               # The title field is updated in the feed table
-            feed.update_attribute :description, @description    # The description field is updated in the feed table
-            feed.update_attribute :link, @link                  # The link field is updated in the feed table
-            flash[:notice] = "New feed added. Articles from this feed will be fetched within the hour."
-          else
-            flash[:warning] = "Invalid URL !"
-            feed.destroy
-            feed.save
-          end
+
+        # If FeedTools guess the feed type it means that the remote document is really a feed
+        if f.feed_type
+          # Perform here actions specific to true feeds...
+          feed.feed_type = f.feed_type
+          # Force feed URL update because sometimes FeedTool is able to found the feed embedded in a static page automaticaly.
+          feed.url = f.url
+
+        # URL is not pointing to something that look like a feed.
         else
-          flash[:warning] = "Invalid URL !"
-          @feed = feed
+          # Let us consider the URL a static page
+          feed.feed_type = "static"
+
+          # Let FeedTool re-parse our brand new feed
+          f = FeedTools::Feed.new
+          # Load FeedTool with our plain feed data using "vanilla" url
+          f.feed_data = get_feed_from_static_page(feed.url(bypass_dynamic_translation = true))
         end
+
+        # In FeedTools we trust !
+        feed.link        = f.link
+        feed.title       = f.title
+        feed.description = f.description
+
+        # Do not add a feed that already exist in database
+        if Feed.find(:all, :conditions => {:url => feed.url(bypass_dynamic_translation = true)}).size > 0
+          flash[:notice] = "This feed was already added to database."
+        else
+          feed.save  # Save Object
+          flash[:notice] = "New feed added. Articles from this feed will be fetched within the hour."
+        end
+
       else
-        flash[:notice] = "This feed was already added to database."
+        flash[:warning] = "Invalid URL !"
+        @feed = feed # TODO: Isn't it what should be done to get back all user input when we redirect to the form ? If so, why it doesn't work as expected ?
       end
-      redirect_to :controller => 'feed' # Go back to default feed view
+
+      # Redirect to the right place
+      if flash[:warning]
+        redirect_to :controller => 'feed', :action => 'add'
+      else
+        redirect_to :controller => 'feed' # Go back to default feed view
+      end
+
     end
   end
 
-  # Method for parsing an RSS feed, using the FeedTools parser
-  def rss(url)
-    ret = false
-    feed = FeedTools::Feed.open(url)  # Creation of an instance of FeedTools and opening of the feed
-    # If the parsing return nil title field, we consider the feed not valid
-    unless feed.title.nil?
-      @title       = feed.title        # We get the title field and convert it to unicode if needed
-      @description = feed.description  # We get the description field and convert it to unicode if needed
-      @link        = feed.link         # We get the link field
-      ret = true        # The return value is set to true (the field is valid)
+
+  # Method that wrap a static page in a feed
+  # Ex: to feedalize the google home page, go to http://www.mydomain.com/feedalize/http://www.google.com
+  def feedalize
+    # Trick required to let RoR parse URLs with options given as parameter in routes
+    self.rebuild_dynamic_parameters
+
+    # Build the feed
+    feed_data = get_feed_from_static_page(@uri)
+
+    # Render the feed
+    if feed_data.blank?
+      flash[:warning] = "No page given to feedalize !"
+      redirect_to :controller => 'feed'
+    else
+      render :text => feed_data # , :type => "application/pdf" # TODO: make sure http header is good (especially the "application/rss+xml" mime type)
     end
-    return ret
   end
+
+
+  # TODO: XML interface ??
+  #        render :xml => Feed.find(:all).to_xml
+
 
   # Method that delete a feed from the database
   def delete
@@ -63,6 +101,69 @@ class FeedController < ApplicationController
       flash[:notice] = "Feed removed !"
     end
     redirect_to :controller => 'feed' # Go back to default feed view
+  end
+
+
+protected
+
+  # When a source URL is given to the feedalize action as a parameter,
+  #  this method help us to keep the entire URL intact while taking care of
+  #  trailing options (like "?foo=bar&option") that RoR ignore by default.
+  # Based on http://www.igvita.com/blog/2007/07/31/reconstructing-request-uris-in-rails/
+  def rebuild_dynamic_parameters
+    # assuming the route with /.*/ match is present, and points to :source
+    @uri = params[:source]
+    # omit true Rails query parameters, and append the rest back to the URI
+    query_string = params.select {|k,v| not %w[action controller source].include?(k)}
+    unless query_string.empty?
+      query_params = query_string.collect {|k,v| "#{k}=#{v}"}
+      logger.info "!!!parameter trick: " + query_params.inspect
+      @uri << "?" << query_params.join("&")
+    end
+  end
+
+
+private
+
+  # Method to transform a static page to a RSS 1.0 feed
+  def get_feed_from_static_page(url)
+    feed_xml = nil
+
+    if url.size > 0  # TODO: perform other validation stuff
+      # The date of the feed / article is when we fetch the page
+      date = Time.now
+
+      # Fetch static page
+      @page = Feedalizer.new(url)  # TODO: is it necessary to declare page as instance variable ?
+
+      # Tiny method to get the inner content of the first html tag given as parameter
+      def getHtmlTagContent(tag)
+        content = ""
+        tag_list = @page.source.search(tag)
+        if tag_list.size > 0
+          content = tag_list.first.inner_html
+        end
+        return content
+      end
+
+      # Set feed attributes using static page attributes
+      @page.feed.about       = url
+      @page.feed.title       = getHtmlTagContent("title")
+      @page.feed.description = getHtmlTagContent("description")
+
+      # Make content of the first <body> tag the only feed item
+      @page.scrape_items("body", limit = 1) do |rss_item, html_element|
+        rss_item.link  = url
+        rss_item.date  = date
+        rss_item.title = @page.feed.title  # + " | " + rss_item.date.strftime(DATE_FORMAT) XXX Not a good idea to add date in title
+        rss_item.description = html_element.inner_html  # TODO: sanitize html content ?
+      end
+
+      # Output RSS 1.0 XML content
+      feed_xml = @page.output
+    end
+
+    return feed_xml
   end
 
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,11 +1,31 @@
 class Feed < ActiveRecord::Base
+
   has_many :trackers
+
+
+  # Feed URL is a lind of a primary key
+  # TODO: should be names "URI" as this acronym reflect the true unique nature of feed access path
+  validates_presence_of :url
+  validates_uniqueness_of :url
+
+
   # Inspired by http://www.nshb.net/node/252
-
   # We should use a combination of FeedTools nomalize method (which take care of URL of the rss:// and feed:// form) and http://www.igvita.com/blog/2006/09/07/validating-url-in-ruby-on-rails/
-
   # As you can see in feed_tools/feed.rb at line 302, FeedTools doesn't support yet https:// and ftp:// protocol. OTOH, we don't whant to allow parsing of local files (via the file:// protocol). So the regexp below only accept http:// URLs.
-  validates_format_of :url,
-                      :with => /^http:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix,
-                      :message => "Malformated URL."
+#   validates_format_of :url,
+#                       :with => /^http:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix,
+#                       :message => "Malformated URL."
+
+
+  # Override default url getter to use our internal feedalizer tool to transform the static page to a feed.
+  # We can still get the true url thanks to the bypass_dynamic_translation parameter.
+  def url(bypass_dynamic_translation=false)
+    url = self[:url]
+    if (not bypass_dynamic_translation) and self[:feed_type] == "static"
+      return "http://localhost:3000/feedalize/#{url}"  # TODO: must be dynamic !
+    else
+      return url
+    end
+  end
+
 end
