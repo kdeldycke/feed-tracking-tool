80bfa5a53abb8c5b70dbc15ab0562a9ae04b0061
Kevin Deldycke <kevin@deldycke.com>
2007-09-21 15:27:32 +0000

    Add tiny method to print the feed state

--- app/helpers/feed_helper.rb
+++ app/helpers/feed_helper.rb
@@ -1,2 +1,11 @@
 module FeedHelper
+
+  # Method that return a pretty formatted string that tell in which status the feed is
+  def pretty_state(feed)
+    if feed.is_pending? or feed.is_static?
+      return feed.state.capitalize
+    end
+    return feed.state.upcase
+  end
+
 end
