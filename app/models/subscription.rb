0c71fea12706399e11748dd022dc55238925b153
Kevin Deldycke <kevin@deldycke.com>
2007-09-21 10:31:21 +0000

    Code layout...

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,14 +1,19 @@
 class Subscription < ActiveRecord::Base
+
   belongs_to :tracker
   belongs_to :profile
 
   # TODO: un-duplicate code below from profile model
   validates_presence_of :frequency
   validates_numericality_of :frequency
-  protected
+
+
+protected
+
   def validate
     unless frequency.nil?
       errors.add(:frequency, "must be comprised between 1 and 31 days") unless (frequency > 0 and frequency < 32)
     end
   end
+
 end
